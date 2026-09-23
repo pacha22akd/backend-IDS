cce08f01d63e02e19138ba18434dc7d58d5faa5f
pacha22akd <mfebles@fi.uba.ar>
2026-09-23 20:39:56 -0300

    agrego la variable 'db' en config.py

--- src/config.py
+++ src/config.py
@@ -1,5 +1,6 @@
 import os #me da acceso a las variables de entorno
 from dotenv import load_dotenv #me permite cargar el archivo .env
+from flask_sqlalchemy import SQLAlchemy
 
 load_dotenv() #carga las variables de entorno definivas en el .env
 
@@ -13,13 +14,12 @@ DB_PASSWORD = os.getenv('DB_PASSWORD', 'root')
 DB_NAME = os.getenv('DB_NAME', 'club_deportivo')
 
 #URI que flask-sqlalchemy lee automaticamente
-SQLALCHEMY_DATABASE_URI = (
-    f'mysql+mysqlconnector://{DB_USER}:{DB_PASSWORD}'
-    f'@{DB_HOST}:{DB_PORT}/{DB_NAME}'
-)
+SQLALCHEMY_DATABASE_URI = f'mysql+mysqlconnector://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}'
+    
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 
-#DEFINIR EL RESTO DE VARIABLES DE ENTORNO ACA
+# Creo el objeto db vacio para que pueda utilizarse y asociarse con la bd
+db = SQLAlchemy()
 
 # Formato de fecha esperado por la API
 FORMATO_FECHA = '%Y-%m-%d'
